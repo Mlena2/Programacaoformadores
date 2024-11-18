a660e7e19317ff806dc59502d8763743949e0cec
Mlena2 <145142712+Mlena2@users.noreply.github.com>
2024-11-18 15:40:44 +0000

    18/11/2024

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Anime',
+    'Anime mais famoso?',
+    'Naruto'
 )
 
 criaCartao(
-    'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação.'
+    'Anime',
+    'O que é shonen?',
+    'Gênero para jovens homens.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é CSS?',
-    'O CSS é uma lingugem de estilização!'
+    'Anime',
+    'Criador de One Piece?',
+    'Eiichiro Oda'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+    'Anime',
+    'Protagonista caçador de demônios?',
+    'Tanjiro de "Demon Slayer".'
 )
 criaCartao(
-    'Programação',
-    'O que é VSCode?',
-    'O Visual Studio Code é um editor de código com diversas integrações.s'
+    'Anime',
+    'Significado de isekai?',
+    'Outro mundo.'
 )
 criaCartao(
-    'Programação',
-    'O que é GitHub?',
-    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+    'Anime',
+    'Anime sobre jogos virtuais?',
+    'Sword Art Online.'
 )
